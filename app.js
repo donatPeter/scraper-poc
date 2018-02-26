08eb6eb9278dcd56cf5cfd9c76fa3b5fdd75349a
donatPeter <peterdonat.bme@gmail.com>
2018-02-26 10:15:55 +0100

    move scraper to a separate file

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const { scrapCV } = require('./utils/scraper');
 
 // Express middleware for routing
 const app = express();
@@ -16,12 +17,7 @@ app.get('/', (req, res) => {
 app.post('*', (req, res) => {
   scrapCV().then(() => {
     res.render('index', {
-      fname: cvMap.fname,
-      lname: cvMap.lname,
-      phone: cvMap.phone,
-      mail: cvMap.mail
     });
-
   })
 });
 
@@ -37,44 +33,4 @@ app.get('*', (req, res) => {
 
 // Init local server
 const port = 5000;
-app.listen(port, () => {
-
-});
-
-// Init selenium-webdriver
-const webdriver = require('selenium-webdriver');
-const chrome = require('selenium-webdriver/chrome')
-const { By, until } = webdriver
-
-// Use headless chrome to not display browser during scraping
-const options = new chrome.Options();
-options.addArguments('headless');
-options.addArguments('disable-gpu')
-
-const path = require('chromedriver').path;
-const service = new chrome.ServiceBuilder(path).build();
-chrome.setDefaultService(service);
-
-// Scraping the example page
-const cvMap = {};
-async function scrapCV() {
-  const driver = new webdriver.Builder()
-    .forBrowser('chrome')
-    .withCapabilities(webdriver.Capabilities.chrome())
-    .setChromeOptions(options)
-    .build();
-  try {
-    await driver.get('https://www.profession.hu/oneletrajz/palyakezdo-marketinges-angol-cv');
-    await driver.findElement(By.css('.info > h2')).getText().then(res => {
-      resArr = res.split(' ');
-      cvMap.fname = resArr[0];
-      cvMap.lname = resArr[1]
-    })
-    await driver.findElement(By.css('.info')).getAttribute('innerText').then(res => {
-      cvMap.phone = res.slice(res.indexOf('Phone'), res.indexOf('E-mail')).replace('Phone: ', '').replace(/ /g, '');
-      cvMap.mail = res.slice(res.indexOf('E-mail')).replace('E-mail: ', '');
-    })
-  } finally {
-    await driver.quit();
-  }
-};
+app.listen(port, () => { });
---
+++ utils/scraper.js
@@ -0,0 +1,40 @@
+// Init selenium-webdriver
+const webdriver = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+const { By, until } = webdriver;
+const path = require('chromedriver').path;
+
+// Use headless chrome to not display browser during scraping
+const options = new chrome.Options();
+options.addArguments('headless');
+options.addArguments('disable-gpu')
+const service = new chrome.ServiceBuilder(path).build();
+chrome.setDefaultService(service);
+
+module.exports = {
+  scrapCV: async function (req, res, next) {
+    // Scraping the example page
+    const cvMap = {};
+    const driver = new webdriver.Builder()
+      .forBrowser('chrome')
+      .withCapabilities(webdriver.Capabilities.chrome())
+      .setChromeOptions(options)
+      .build();
+    try {
+      await driver.get('https://www.profession.hu/oneletrajz/palyakezdo-marketinges-angol-cv');
+      await driver.findElement(By.css('.info > h2')).getText().then(res => {
+        resArr = res.split(' ');
+        cvMap.fname = resArr[0];
+        cvMap.lname = resArr[1]
+      })
+      await driver.findElement(By.css('.info')).getAttribute('innerText').then(res => {
+        cvMap.phone = res.slice(res.indexOf('Phone'), res.indexOf('E-mail')).replace('Phone: ', '').replace(/ /g, '');
+        cvMap.mail = res.slice(res.indexOf('E-mail')).replace('E-mail: ', '');
+      })
+    } finally {
+      await driver.quit();
+    }
+  }
+}
+
+
