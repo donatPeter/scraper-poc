d0f05cda9a39d56f24a5e57c2ca7a43edca7e8fc
donatPeter <peterdonat.bme@gmail.com>
2018-02-26 12:24:58 +0100

    ignore dev keys file

--- config/passport.js
+++ config/passport.js
@@ -2,25 +2,24 @@ const LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
 const keys = require('./keys');
 
 module.exports = function (passport) {
-  
+
   passport.use(new LinkedInStrategy({
     clientID: keys.ClientID,
     clientSecret: keys.ClientSecret,
     callbackURL: "http://localhost:5000/auth/linkedin/callback",
     scope: ['r_emailaddress', 'r_basicprofile'],
-    state: true,
-    profileFields: ['id', 'first-name', 'last-name', 'email-address', 'headline']
+    state: true
   }, function (accessToken, refreshToken, profile, done) {
     process.nextTick(function () {
       return done(null, profile);
     });
   }));
 
-  passport.serializeUser(function(user, done) {
+  passport.serializeUser(function (user, done) {
     done(null, user.id);
   });
 
-  passport.deserializeUser(function(id, done) {
+  passport.deserializeUser(function (id, done) {
     done(null, false);
   });
 }
